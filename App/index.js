281a438e28d0f6bd763e8b67d08d6baf57e3a22e
Timothy Lim <timothylim.student@gmail.com>
2020-12-02 19:52:39 -0300

    Fix

--- App/index.js
+++ App/index.js
@@ -1,5 +1,41 @@
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+// import React from "react";
+// import { NavigationContainer } from "@react-navigation/native";
+// import { createStackNavigator } from "@react-navigation/stack";
 
-import SignIn from "genshin-impact-app/App/screens/SignIn";
+// import SignIn from "genshin-impact-app/App/screens/SignIn";
+
+import React from "react";
+import { View, Text, Button } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+} from "@react-navigation/drawer";
+
+import Home from "genshin-impact-app/App/screens/Home";
+import Threads from "genshin-impact-app/App/screens/Threads";
+import TierList from "genshin-impact-app/App/screens/TierList";
+
+const Drawer = createDrawerNavigator();
+
+function MyDrawer() {
+  return (
+    <Drawer.Navigator
+    // drawerContent={(props) => <CustomDrawerContent {...props} />}
+    >
+      <Drawer.Screen name="Home" component={Home} />
+      <Drawer.Screen name="Threads" component={Threads} />
+      <Drawer.Screen name="Tier List" component={TierList} />
+    </Drawer.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyDrawer />
+    </NavigationContainer>
+  );
+}
