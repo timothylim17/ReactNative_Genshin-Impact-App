17d6ed2bf458cc851bf803a765a903d2dc7552f0
Cy Bunayog <cybunayog@gmail.com>
2020-12-03 19:27:56 -0800

    Creating sign in

--- App/screens/Home.js
+++ App/screens/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { View, Text } from "react-native";
 
-export default function CharacterInfo({ navigation }) {
+export default function Home({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Home!</Text>
--- App/screens/SignIn.js
+++ App/screens/SignIn.js
@@ -7,7 +7,8 @@ import {
   TouchableOpacity,
   ActivityIndicator,
   KeyboardAvoidingView,
-  Keyboard
+  Keyboard,
+  TouchableWithoutFeedback
 } from "react-native";
 
 import * as Google from 'expo-google-app-auth';
@@ -25,11 +26,81 @@ const androidClientId = {
   ANDROID_CLIENT_ID
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center'
+  }
+})
+
 export default function SignIn({ navigation }) {
+  const [email, password, errorMessage, setErrorMessage] = useState('');
+  const [isLoading] = useState(false);
+
+  // Login successful
+  const onLoginSuccess = () => {
+    navigation.navigate('Home');
+  }
+
+  // Login failed
+  const onLoginFailure = e => {
+    setErrorMessage(e);
+    isLoading(false);
+  }
+
+  // Sign in with Email
+  async function signInWithEmail() {
+    await firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(() => {
+        onLoginSuccess();
+      })
+      .catch(e => {
+        let errorCode = e.code;
+        let errorMessage = e.message;
+
+        if (errorCode == 'auth/weak-password')
+          onLoginFailure('Weak Password!');
+        else
+          onLoginFailure(errorMessage);
+      })
+  }
+
+  async function signInWithGoogleAsync() {
+    try {
+      const result = await Google.logInAsync({
+        androidClientId,
+        iosClientId,
+        behavior: 'web',
+        scopes: ['profile', 'email']
+      });
+
+      if (result.type === 'success') {
+        await firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL);
+        const credential = firebase.auth.GoogleAuthProvider.credential(data.idToken, data.accessToken);
+        const googleProfileData = await firebase.auth().signInWithCredential(credential);
+        onLoginSuccess();
+      }
+    } catch ({ message }) {
+      alert('login: Error: ' + message);
+    }
+  }
 
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Sign In</Text>
-    </View>
+    <TouchableWithoutFeedback
+      onPress={() => {
+        Keyboard.dismiss();
+      }}
+    >
+      <SafeAreaView style={{ flex: 1 }}>
+        <KeyboardAvoidingView style={styles.container} behavior="padding">
+          <Text style={{ fontSize: 32, fontWeight: "700", color: "#000", paddingTop: 40 }}>
+            Genshin Impact
+          </Text>
+        </KeyboardAvoidingView>
+      </SafeAreaView>
+    </TouchableWithoutFeedback>
   );
 }
