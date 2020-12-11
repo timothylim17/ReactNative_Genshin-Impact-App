030d1062e7045dfbb37319583d769dd4581cb725
Cy Bunayog <cybunayog@gmail.com>
2020-12-10 17:27:51 -0800

    Added home ui

--- App/screens/Home.js
+++ App/screens/Home.js
@@ -1,31 +1,58 @@
-import React, { useContext } from "react";
-import { SafeAreaView, Text, Button, StyleSheet, ScrollView } from "react-native";
+import React from "react";
+import { SafeAreaView, Text, TouchableOpacity, StyleSheet, ScrollView, View } from "react-native";
 import * as firebase from 'firebase';
-
-import { AuthContext } from 'genshin-impact-app/App/modules/navigation';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: '#222431'
   },
   welcomeUserText: {
-    marginTop: 20,
+    marginVertical: 90,
     color: '#fff',
     textAlign: 'center',
-    fontSize: 20
+    fontSize: 30
+  },
+  text: {
+    color: '#fff',
+  },
+  optionsView: {
+    flexDirection: 'row',
+    marginTop: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+    backgroundColor: '#20212c',
+    paddingHorizontal: 100,
+    height: 100,
   }
 });
 
 
 export default function Home({ navigation }) {
-  const { signOut } = useContext(AuthContext);
+
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
         <Text style={styles.welcomeUserText}>Welcome {firebase.auth().currentUser.displayName ? firebase.auth().currentUser.displayName : ''}!</Text>
+        <View style={styles.optionsView}>
+          <TouchableOpacity onPress={() => navigation.navigate('Threads')}>
+            <Text style={styles.text}>Go to Threads!</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.optionsView}>
+          <TouchableOpacity onPress={() => navigation.navigate('Tier List')}>
+            <Text style={styles.text}>Go to Tier List!</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.optionsView}>
+          <TouchableOpacity onPress={() => console.log('TODO')}>
+            <Text style={styles.text}>Webview to Genshin.gg!</Text>
+          </TouchableOpacity>
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
